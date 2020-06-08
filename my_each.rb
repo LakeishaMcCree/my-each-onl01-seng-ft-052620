e5f86dbc9a0e46b4b8f8ca9cc49bd2c6526e1ebf
Lakeisha McCree <lmccree215@icloud.com>
2020-06-08 14:01:36 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,14 @@
 def my_each(collection)
   counter = 0
+<<<<<<< HEAD
   while counter < collection.length
     yield collection[counter]
     counter += 1
   end
   collection
+=======
+  while counter < collection.length 
+  end 
+  
+>>>>>>> dd29bed79c173f8d556d2cd8f410ad446977647e
 end
